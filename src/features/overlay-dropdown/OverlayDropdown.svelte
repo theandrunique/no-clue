a84c3ecdaa17c537b7ec6f8238cab0dd32404ce7
theandrunique <theandrunique@gmail.com>
2026-08-14 16:13:55 +0500

    Add transition and fix scroll problem on dropdown opening

--- src/features/overlay-dropdown/OverlayDropdown.svelte
+++ src/features/overlay-dropdown/OverlayDropdown.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
   import { Card } from "$lib/components/ui";
   import { overlayStateStore } from "$lib/stores/overlayStateStore.svelte";
+  import { cubicInOut } from "svelte/easing";
+  import { fly } from "svelte/transition";
 </script>
 
 {#if overlayStateStore.expanded}
-  <Card class="flex-1 h-full bg-(--bg-card)/50">
-    Some text goes here
-  </Card>
+  <div transition:fly={{ y: 16, duration: 200, easing: cubicInOut }} class="flex-1 min-h-0">
+    <Card class="h-full bg-(--bg-card)/50">
+      Some text goes here
+    </Card>
+  </div>
 {/if}
--- src/routes/overlay/+page.svelte
+++ src/routes/overlay/+page.svelte
@@ -3,7 +3,7 @@
   import { OverlayMenu } from "$features/overlay-menu";
 </script>
 
-<div class="h-screen flex flex-col gap-1">
+<div class="h-screen flex flex-col gap-1 overflow-hidden">
   <OverlayMenu />
 
   <OverlayDropdown />
